20212408e25b0d8cc1f1134d08cfa8d9f9137e00
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-03-04 19:44:30 +0400

    added enemy behavior and damage

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+  [SerializeField]
+  private float _speed = 4f;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // move down at 4 m/s
+    var direction = new Vector3(0, -1, 0);
+    transform.Translate(direction * _speed * Time.deltaTime);
+
+    // if bottom of screen
+    // respawn at top with a new random x position
+    if (transform.position.y < -5.5f)
+    {
+      float randomX = Random.Range(-9.5f, 9.5f);
+      transform.position = new Vector3(randomX, 7, 0);
+    }
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.tag == "Player")
+    {
+      Player player = other.transform.GetComponent<Player>();
+      if (player != null)
+      {
+        player.Damage();
+      }
+      Destroy(this.gameObject);
+    }
+    if (other.tag == "Laser")
+    {
+      Destroy(other.gameObject);
+      Destroy(this.gameObject);
+    }
+  }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,6 +15,9 @@ public class Player : MonoBehaviour
   [SerializeField]
   private GameObject _laserPrefab;
 
+  [SerializeField]
+  private int _lives = 3;
+
 
   // Start is called before the first frame update
   void Start()
@@ -81,6 +84,15 @@ public class Player : MonoBehaviour
     Instantiate(_laserPrefab,
                   transform.position + new Vector3(0, 0.8f, 0),
                   Quaternion.identity);
+  }
+
+  public void Damage()
+  {
+    _lives--;
+    if (_lives < 1)
+    {
+      Destroy(this.gameObject);
+    }
   }
 
 }
