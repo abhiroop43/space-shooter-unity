adcedce1eed373b22ca6fb6ffd1eb1effd53b1c4
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-03-09 23:02:02 +0400

    added laser

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -89,7 +89,7 @@ public class Player : MonoBehaviour
 
     // Instantiate the laser prefab
     Instantiate(_laserPrefab,
-                  transform.position + new Vector3(0, 0.8f, 0),
+                  transform.position + new Vector3(0, 1.05f, 0),
                   Quaternion.identity);
   }
 
